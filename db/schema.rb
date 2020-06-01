f5fb0fca09a771748626e388901e770b5567bec2
Alex Frezoul <frezoul.alex@gmail.com>
2020-06-01 15:51:46 +0200

    doc: theme body: thème implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_093650) do
+ActiveRecord::Schema.define(version: 2020_06_01_130009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "indices", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "title"
