fe2270a40e2b363eb6a108ef480d5e0418098670
jessica leclercq <jessica_leclercq@hotmail.fr>
2020-06-02 10:01:16 +0200

    doc:navabar body:added a navbar

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 2020_06_01_164020) do
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
+  create_table "indices", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "description"
