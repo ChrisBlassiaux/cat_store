737c58f630a6c72e661b2edd289df85fbca50a4a
jessica leclercq <jessica_leclercq@hotmail.fr>
2020-06-02 12:57:11 +0200

    fix: authenticate_user

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user, except: [:index]
+  before_action :authenticate_user!, except: [:index]
 
   def index
     @items = Item.all
