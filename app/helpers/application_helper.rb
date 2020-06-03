b0730c7ba3f99f331a4660729a4da6f0f42c0830
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-06-03 23:20:17 +0200

    style: font/ traduction etc

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
   def bootstrap_class_for_flash(type)
-  case type
-    when 'notice' then "alert-info"
-    when 'success' then "alert-success"
-    when 'error' then "alert-danger"
-    when 'alert' then "alert-warning"
+    case type
+      when 'notice' then "alert-info"
+      when 'success' then "alert-success"
+      when 'error' then "alert-danger"
+      when 'alert' then "alert-warning"
+    end
   end
-end
 
 end
