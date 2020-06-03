267541edef33f76c18ea9fb1e2dd02c7374bb7b6
jessica leclercq <jessica_leclercq@hotmail.fr>
2020-06-03 14:39:54 +0200

    fonctionnalites front

--- db/schema.rb
+++ db/schema.rb
@@ -74,7 +74,6 @@ ActiveRecord::Schema.define(version: 2020_06_03_083703) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
