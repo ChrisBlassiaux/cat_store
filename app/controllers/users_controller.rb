6f00c8d6de9a9ccdb6895e0b31b2f1dd33593d31
Alex Frezoul <frezoul.alex@gmail.com>
2020-06-04 17:50:24 +0200

    css: modify comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
-
   end
 
   def edit
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_081526) do
+ActiveRecord::Schema.define(version: 2020_06_04_144052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,12 +23,13 @@ ActiveRecord::Schema.define(version: 2020_06_04_081526) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "first_name"
     t.text "body"
     t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["item_id"], name: "index_comments_on_item_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "indices", force: :cascade do |t|
@@ -89,6 +90,7 @@ ActiveRecord::Schema.define(version: 2020_06_04_081526) do
   end
 
   add_foreign_key "comments", "items"
+  add_foreign_key "comments", "users"
   add_foreign_key "join_table_cart_items", "carts"
   add_foreign_key "join_table_cart_items", "items"
 end
